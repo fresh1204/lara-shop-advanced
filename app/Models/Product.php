43614dc4db4d19000e580fc2377111b20317b96e
vagrant <vagrant@homestead>
2019-03-07 16:31:00 +0000

    初始化众筹模型

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,10 +7,18 @@ use Illuminate\Support\Str;
 
 class Product extends Model
 {
-    //
+    //商品类型
+    const TYPE_NORMAL = 'normal';
+    const TYPE_CROWDFUNDING = 'crowdfunding';
+    public static $typeMap = [
+        self::TYPE_NORMAL  => '普通商品',
+        self::TYPE_CROWDFUNDING => '众筹商品',
+    ];
+
     protected $fillable = [
     	'title','description','image','on_sale',
-    	'rating','sold_count','review_count','price' 
+    	'rating','sold_count','review_count','price',
+        'type',
     ];
 
     protected $casts = [
@@ -42,4 +50,9 @@ class Product extends Model
         return $this->belongsTo(Category::class);
     }
 
+    //与众筹商品模型的关联关系
+    public function crowdfunding()
+    {
+        return $this->hasOne(crowdfundingProduct::class);
+    }
 }
