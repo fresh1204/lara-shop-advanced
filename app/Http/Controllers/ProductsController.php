6f7e3052c59cfd5f6747f68143d4a31f86e252f8
fresh1204 <1942480291@qq.com>
2019-03-15 09:14:57 +0000

    封装 Elasticsearch 查询代码,推荐相似商品

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -10,6 +10,8 @@ use App\Models\OrderItem;
 use App\Models\Category;
 use App\Services\CategoryService;
 use Illuminate\Pagination\LengthAwarePaginator;
+use App\SearchBuilders\ProductsSearchBuilder;
+use App\Services\ProductService;
 
 class ProductsController extends Controller
 {
@@ -86,6 +88,7 @@ class ProductsController extends Controller
         $page = $request->input('page',1);
         $perPage = 16;
 
+        /*
         // 构建查询
         $params = [
             'index' => 'products',
@@ -102,12 +105,17 @@ class ProductsController extends Controller
                 ],
             ],
         ];
+        */
+
+        // 新建查询构造器对象，设置只搜索上架商品，设置分页
+        $builder = (new ProductsSearchBuilder())->onSale()->paginate($perPage,$page);
 
         // 关键词搜索
         if($search = $request->input('search','')){
             // 将搜索词根据空格拆分成数组，并过滤掉空项
             $keywords = array_filter(explode(' ',$search));
 
+            /*该部分已封装
             $params['body']['query']['bool']['must'] = [];
             // 遍历搜索词数组，分别添加到 must 查询中
             foreach($keywords as $keyword){
@@ -128,11 +136,15 @@ class ProductsController extends Controller
                     ]
                 ];
             }
+            */
+            // 调用查询构造器的关键词筛选
+            $builder->keywords($keywords);
             
         }
 
         // 商品按类目筛选
         if($request->input('category_id') && $category = Category::find($request->input('category_id'))){
+            /*
             if($category->is_directory){
                 // 如果是一个父类目，则使用 category_path 来筛选
                 $params['body']['query']['bool']['filter'][] = [
@@ -144,10 +156,14 @@ class ProductsController extends Controller
                     'term' => ['category_id' => $category->id]
                 ];
             }
+            */
+            // 调用查询构造器的类目筛选
+            $builder->category($category);
         }
 
         // 只有当用户有输入搜索词或者使用了类目筛选的时候才会做聚合
         if($search || isset($category)){
+            /*
             $params['body']['aggs'] = [
                 'properties' => [
                     'nested' => [
@@ -169,6 +185,10 @@ class ProductsController extends Controller
                     ],
                 ]
             ];
+            */
+
+            // 调用查询构造器的分面搜索
+            $builder->aggregateProperties();
         }
 
         $propertyFilters = [];
@@ -178,12 +198,15 @@ class ProductsController extends Controller
             // 将获取到的字符串用符号 | 拆分成数组
             $filterArray = explode('|',$filterString);
             foreach($filterArray as $filter){
+
                 // 将字符串用符号 : 拆分成两部分并且分别赋值给 $name 和 $value 两个变量
                 list($name,$value) = explode(':',$filter);
+
                 //dd($filter);
                 // 将用户筛选的属性添加到数组中
                 $propertyFilters[$name] = $value;
 
+                /*
                 // 添加到 filter 类型中
                 $params['body']['query']['bool']['filter'][] = [
                     // 由于我们要筛选的是 nested 类型下的属性，因此需要用 nested 查询
@@ -198,6 +221,10 @@ class ProductsController extends Controller
                         ],
                     ],
                 ];
+                */
+
+                // 调用查询构造器的属性筛选
+                $builder->propertyFilter($name,$value);
             }
         }
 
@@ -208,23 +235,32 @@ class ProductsController extends Controller
             if(preg_match('/^(.+)_(asc|desc)$/',$order,$m)){
                 // 如果字符串的开头是这 3 个字符串之一，说明是一个合法的排序值
                 if(in_array($m[1], ['price','rating','sold_count'])){
+
                     // 根据传入的排序值来构造排序参数
-                    $params['body']['sort'] = [[$m[1] => $m[2]]];
+                    //$params['body']['sort'] = [[$m[1] => $m[2]]];
+
+                    // 调用查询构造器的排序
+                    $builder->orderBy($m[1], $m[2]);
                 }
             }
         }
 
-        $result = app('es')->search($params);
+        //$result = app('es')->search($params);
+        // 最后通过 getParams() 方法取回构造好的查询参数
+        $result = app('es')->search($builder->getParams());
+
         // 通过 collect 函数将返回结果转为集合，并通过集合的 pluck 方法取到返回的商品 ID 数组
         $productIds = collect($result['hits']['hits'])->pluck('_id')->all();
-        //echo sprintf("FIND_IN_SET(id,'%s')",join(',',$productIds));
+
         // 通过 whereIn 方法从数据库中读取商品数据
+        /*
         $products = Product::query()
             ->whereIn('id',$productIds)
             // orderByRaw 可以让我们用原生的 SQL 来给查询结果排序
-
             ->orderByRaw(sprintf("FIND_IN_SET(id,'%s')",join(',',$productIds)))
             ->get();
+        */
+        $products = Product::query()->byIds($productIds)->get();
         // 返回一个 LengthAwarePaginator 对象
         $pager = new LengthAwarePaginator($products,$result['hits']['total'],$perPage,$page,[
             'path' => route('products.index',false), // 手动构建分页的 url
@@ -260,8 +296,8 @@ class ProductsController extends Controller
         ]);
     }
 
-    //商品详情
-    public function show(Product $product,Request $request)
+    //商品详情        (利用 Laravel 的自动注入来初始化 ProductService 对象)
+    public function show(Product $product,Request $request,ProductService $service)
     {
     	//判断商品是否已经上架，如果没上架，则抛出异常
     	if(! $product->on_sale){
@@ -287,8 +323,7 @@ class ProductsController extends Controller
                 $emailActive = 0;
             }
     	}
-        //var_dump($favored);exit;
-
+        
         $reviews = OrderItem::query()
             ->with(['order.user','productSku']) // 预先加载关联关系
             ->where('product_id',$product->id)
@@ -296,12 +331,45 @@ class ProductsController extends Controller
             ->orderBy('reviewed_at','desc')
             ->limit(10)
             ->get();
-        //echo '<pre>';print_r($reviews->toArray());exit;
+
+        /* 此部分已封装在 getSimilarProductIds（）中
+        // 创建一个查询构造器，只搜索上架的商品，取搜索结果的前 4 个商品
+        $builder = (new ProductsSearchBuilder())->onSale()->paginate(4,1);
+        // 遍历当前商品的属性
+        foreach($product->properties as $property){
+            // 添加到 should 条件中
+            $builder->propertyFilter($property->name,$property->value,'should');
+        }
+
+        // 设置最少匹配一半属性
+        $builder->minShouldMatch(ceil(count($product->properties) / 2));
+        $params = $builder->getParams();
+        
+        // 同时将当前商品的 ID 排除
+        $params['body']['query']['bool']['must_not'] = [['term' => ['_id' => $product->id]]];
+
+        //搜索
+        $result = app('es')->search($params);
+        //echo '<pre>';print_r($result);exit;
+        
+        $similarProductIds = collect($result['hits']['hits'])->pluck('_id')->all();
+        */
+
+        $similarProductIds = $service->getSimilarProductIds($product, 4);
+        // 根据 Elasticsearch 搜索出来的商品 ID 从数据库中读取商品数据
+        /*
+        $similarProducts = Product::query()
+            ->whereIn('id',$similarProductIds)
+            ->orderByRaw(sprintf("FIND_IN_SET(id, '%s')",join(',',$similarProductIds)))
+            ->get();
+       */
+        $similarProducts = Product::query()->byIds($similarProductIds)->get();
     	return view('products.show',[
             'product' => $product,
             'favored' => $favored,
             'emailActive' => $emailActive,
-            'reviews' => $reviews
+            'reviews' => $reviews,
+            'similar' => $similarProducts,
         ]);
     }
 
