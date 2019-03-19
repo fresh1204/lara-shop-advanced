d335c52251ef05b446ad7ec23e301ed3de908eda
fresh1204 <1942480291@qq.com>
2019-03-19 01:35:04 +0000

    Elasticsearch 索引迁移命令

--- app/Console/Commands/Elasticsearch/Migrate.php
+++ app/Console/Commands/Elasticsearch/Migrate.php
@@ -38,6 +38,7 @@ class Migrate extends Command
 
         //索引类数组，先留空
         $indices = [];
+        $indices = [Indices\ProjectIndex::class];
         // 遍历索引类数组
         foreach($indices as $indexClass){
             // 调用类数组的 getAliasName() 方法来获取索引别名
@@ -93,12 +94,60 @@ class Migrate extends Command
     // 更新已有的索引
     protected function updateIndex($aliasName,$indexClass)
     {
-
+        // 暂时关闭索引
+        $this->es->indices()->close(['index'=>$aliasName]);
+        // 更新索引设置
+        $this->es->indices()->putSettings([
+            'index' => $aliasName,
+            'body'  => $indexClass::getSettings(),
+        ]);
+        // 更新索引字段
+        $this->es->indices()->putMapping([
+            'index' => $aliasName,
+            'type'  => '_doc',
+            'body'  => [
+                '_doc' => [
+                    'properties' => $indexClass::getProperties(),
+                ],
+            ],
+        ]);
+        // 重新打开索引
+        $this->es->indices()->open(['index' => $aliasName]);
     }
 
     // 重建索引
     protected function reCreateIndex($aliasName,$indexClass)
     {
-
+         // 获取索引信息，返回结构的 key 为索引名称，value 为别名
+        $indexInfo     = $this->es->indices()->getAliases(['index' => $aliasName]);
+        // 取出第一个 key 即为索引名称
+        $indexName = array_keys($indexInfo)[0];
+        // 用正则判断索引名称是否以 _数字 结尾
+        if (!preg_match('~_(\d+)$~', $indexName, $m)) {
+            $msg = '索引名称不正确:'.$indexName;
+            $this->error($msg);
+            throw new \Exception($msg);
+        }
+        // 新的索引名称
+        $newIndexName = $aliasName.'_'.($m[1] + 1);
+        $this->info('正在创建索引'.$newIndexName);
+        $this->es->indices()->create([
+            'index' => $newIndexName,
+            'body'  => [
+                'settings' => $indexClass::getSettings(),
+                'mappings' => [
+                    '_doc' => [
+                        'properties' => $indexClass::getProperties(),
+                    ],
+                ],
+            ],
+        ]);
+        $this->info('创建成功，准备重建数据');
+        $indexClass::rebuild($newIndexName);
+        $this->info('重建成功，准备修改别名');
+        $this->es->indices()->putAlias(['index' => $newIndexName, 'name' => $aliasName]);
+        $this->info('修改成功，准备删除旧索引');
+        $this->es->indices()->delete(['index' => $indexName]);
+        $this->info('删除成功');
     }
 }
